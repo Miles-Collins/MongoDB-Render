aa1c811c5eaa494a075fcd83aa9bb31207ad74d4
Miles-Collins <miles.collins@ymail.com>
2023-06-29 11:25:57 -0600

    npm i

--- MongoDB-Render/MongoDB-Render.client/src/env.js
+++ MongoDB-Render/MongoDB-Render.client/src/env.js
@@ -1,6 +1,6 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const dev = window.location.origin.includes("localhost");
+export const baseURL = dev ? "http://localhost:3000" : "";
+export const useSockets = false;
+export const domain = "https://dev-tl4ahg055qlcfzyc.us.auth0.com";
+export const clientId = "6OChR2mTlHQTrsAacGVt27X6DijMpskb";
+export const audience = "https://dev-tl4ahg055qlcfzyc.us.auth0.com/api/v2/";
